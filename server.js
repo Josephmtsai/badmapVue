bdd0f170ca5c4beebadc5a51f0217433b025f59d
JosephTsai <mmx362003@gmail.com>
2017-08-07 00:28:46 +0800

    try change server.js

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
-var express = require('express');
-var app = express();
+const connect = require('connect')
+const serveStatic = require('serve-static')
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+const app = connect()
+app.use(serveStatic(__dirname) +"/dist")
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app listen on ${port}`)
