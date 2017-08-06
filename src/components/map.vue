0449799e09be9f7e649d465aae79af2ca2ba595a
JosephTsai <mmx362003@gmail.com>
2017-08-06 13:07:18 +0800

    add select and map

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,47 +1,51 @@
 <template>
   <div  class="text-center">
-    <b-dropdown text="Dropdown Button sm" size="sm" class="m-md-2">
-        <b-dropdown-item href="#">Action</b-dropdown-item>
-        <b-dropdown-item href="#">Another action</b-dropdown-item>
-        <b-dropdown-item href="#">Something else here</b-dropdown-item>
-    </b-dropdown>
-
-    <br>
-    <br>
-
-    <b-dropdown text="Split Dropdown Button" variant="success" split class="m-md-2">
-        <b-dropdown-item href="#">Action</b-dropdown-item>
-        <b-dropdown-item href="#">Another action</b-dropdown-item>
-        <b-dropdown-item href="#">Something else here...</b-dropdown-item>
-    </b-dropdown>
-
-    <br>
-    <br>
-
+    <b-btn variant="success"  ref="btn_click"   @click="getLocation">Get Location  </b-btn>
     <b-dropdown text="Right align" variant="warning" right class="m-md-2">
-        <b-dropdown-item href="#">Action</b-dropdown-item>
-        <b-dropdown-item href="#">Another action</b-dropdown-item>
-        <b-dropdown-item href="#">Something else here</b-dropdown-item>
+        <b-dropdown-item href="#" v-for="location in locationList" v-bind:item="item"   v-bind:index="location"  v-bind:key="location.name" >{{location.name}}</b-dropdown-item>
     </b-dropdown>
 
-    <br>
-    <br>
+    <b-form-select v-model="selected"  value-field="name" text-field="name"  :options="locationList"  class="mb-3" ></b-form-select>
+    <gmap-map   :center="center"  :zoom="zoom"  map-type-id="terrain"  style="width: 500px; height: 300px"></gmap-map>
 
-    <b-dropdown text="Drop-Up" dropup variant="info" class="m-md-2">
-        <b-dropdown-item href="#">Action</b-dropdown-item>
-        <b-dropdown-item href="#">Another action</b-dropdown-item>
-        <b-dropdown-item href="#">Something else here</b-dropdown-item>
-    </b-dropdown>
-
-    <br>
-    <br>
-
-    <b-dropdown text="Link" variant="info" class="m-md-2" link>
-        <b-dropdown-item href="#">Action</b-dropdown-item>
-        <b-dropdown-item href="#">Another action</b-dropdown-item>
-        <b-dropdown-item href="#">Something else here</b-dropdown-item>
-    </b-dropdown>
-
+    {{selected}}
 </div>
 
 </template>
+<script>
+  import Vue from 'vue'
+  import * as VueGoogleMaps from 'vue2-google-maps'
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyBW6jkW1QIMpjw5SUIytKmuQ40emkWLgM8',
+      v: '3'
+    }
+  })
+
+  export default {
+    data () {
+      return {
+        center: { lat: 23.6052631, lng: 120.1412734 },
+        zoom: 13,
+        markers: [{
+          position: {
+            lat: 25.0781209,
+            lng: 121.5730155
+          },
+          infoText: 'Marker 1'
+        }],
+        locationList: [],
+        errorMsg: '',
+        selected: {}
+      }
+    },
+    methods: {
+      getLocation () {
+        const api = 'https://qatbadmap.herokuapp.com/api/locationinfolist'
+        Vue.axios.get(api).then(response => {
+          this.locationList = response.data
+        })
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ Vue.config.productionTip = false
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
