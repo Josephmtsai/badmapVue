403d7a483af2fc14ff2c720d4f62be1af530765a
JosephTsai <mmx362003@gmail.com>
2017-08-10 00:57:05 +0800

    add date and cluster

--- src/components/map.vue
+++ src/components/map.vue
@@ -5,16 +5,21 @@
     </div>
     <div class="col-12" v-if="!loading">
       <div class="col-4">
+        <date-picker v-model="endTime" range></date-picker>
+        {{endTime}}
         <span class="label label-default" for="selectLocation">Specify Location</span>
         <div >
           <b-form-select v-model="selected"  id="selectLocation" value-field="name" text-field="name"  :options="locationList"  class="mb-3" ></b-form-select>
         </div>
+        <button class="danger" @click="showTodayInfo()" >Show Today </button>
       </div>
       <div class="col-4">
   
       </div>      
       <gmap-map  class="col-12" style="height:800px" :center="center"  :zoom="zoom"  map-type-id="roadmap"  >
+        <gmap-cluster >
             <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" @click="showModal(m)" :label="m.infoText"  ></gmap-marker>
+        </gmap-cluster>
       </gmap-map>
 
       <b-modal ref="modal" title="Location Info" @ok="closeModal" >
@@ -27,7 +32,6 @@
 
 <style>
 .overlay {
-    background: #e9e9e9;  
     position: absolute;   
     top: 0;               
     right: 0;             
@@ -42,6 +46,7 @@
   import * as VueGoogleMaps from 'vue2-google-maps'
   import axios from 'axios'
   import HourGlass from 'vue-loading-spinner/src/components/HourGlass.vue'
+  import DatePicker from 'vue2-datepicker'
   Vue.use(VueGoogleMaps, {
     load: {
       key: 'AIzaSyBW6jkW1QIMpjw5SUIytKmuQ40emkWLgM8',
@@ -68,7 +73,10 @@
         selected: '',
         matchLocation: [],
         loading: true,
-        selectedLocation: {}
+        selectedLocation: {},
+        startTime: '',
+        endTime: '',
+        todayWeekday: 0
       }
     },
     created () {
@@ -79,6 +87,8 @@
         self.loading = false
         self.updateMap()
       }))
+      var today = new Date()
+      self.todayWeekday = today.getDay()
     },
     watch: {
       selected: function (val) {
@@ -107,10 +117,17 @@
       showModal (marker) {
         this.selectedLocation = marker
         this.$refs.modal.show()
+      },
+      showTodayInfo () {
+        var self = this
+        this.markers = this.badmintonList.filter(function (location) { return location.weekDayInt === self.todayWeekday })
+        this.center = this.markers[0].position
       }
+
     },
     components: {
-      HourGlass
+      HourGlass,
+      DatePicker
     }
   }
 </script>
